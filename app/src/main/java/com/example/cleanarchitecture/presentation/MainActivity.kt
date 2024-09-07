5c3e2ab9fbc2fdc970a702a6ad1e0f3915408d7c
Trif <Trif@mail.ru>
2024-09-07 22:35:48 +0300

    Create project

---
+++ app/src/main/java/com/example/cleanarchitecture/presentation/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.cleanarchitecture.presentation
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.cleanarchitecture.R
+import com.example.cleanarchitecture.domain.models.SaveUserNameParam
+import com.example.cleanarchitecture.domain.models.UserName
+import com.example.cleanarchitecture.domain.usecase.GetUserNameUseCase
+import com.example.cleanarchitecture.domain.usecase.SaveUserNameUseCase
+
+class MainActivity : Activity() {
+
+    private val getUserNameUseCase = GetUserNameUseCase()
+    private val saveUserNameUseCase = SaveUserNameUseCase()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataTextView = findViewById<TextView>(R.id.dataTextView)
+        val dataEditView = findViewById<EditText>(R.id.dataEditText)
+        val sendButton = findViewById<Button>(R.id.sendButton)
+        val receiveButton = findViewById<Button>(R.id.receiveButton)
+
+        sendButton.setOnClickListener {
+            val text = dataEditView.text.toString()
+            val params = SaveUserNameParam(name = text)
+            val result: Boolean = saveUserNameUseCase.execute(param = params)
+            dataTextView.text = "Save result = $result"
+        }
+
+        receiveButton.setOnClickListener {
+            val userName : UserName = getUserNameUseCase.execute()
+            dataTextView.text = "${userName.firstName} ${userName.lastName}"
+        }
+    }
+}
